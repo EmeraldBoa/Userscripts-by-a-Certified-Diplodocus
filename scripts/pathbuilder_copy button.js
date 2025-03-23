e0f58db112a79e9f2687ff69cdfc0001eb190cd1
EmeraldBoa <0arianwen0@gmail.com>
2025-03-23 23:51:37 +0100

    Pathbuilder: Added template script

---
+++ scripts/pathbuilder_copy button.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Copy button for Pathbuilder
+// @namespace    https://greasyfork.org/en/users/757649-certifieddiplodocus
+// @version      0.1
+// @description  Adds a copy button to Pathbuilder's feat, feature and item descriptions.
+// @author       CertifiedDiplodocus
+// @match        http*://pathbuilder2e.com/app.html*
+// @icon         https://upload.wikimedia.org/wikipedia/commons/8/8c/Cib-archive-of-our-own_%28CoreUI_Icons_v1.0.0%29.svg
+// @license      GPL-3.0-or-later
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+/* TODOs
+        [ ] identify location
+        [ ] jquery or not?
+        [ ] create simple button & add to page
+        [ ] add copy code
+
+// TODO IMPORTANT: Will it work even though the page only loads after the user clicks to load a character?
+
+        Steps: 
+            [ ] 1. Test adding button to feats page
+            [ ] 2. Test on sidebar
+----------------------------------------------------------------------------------------------------------------------*/
+
+(function() {
+    'use strict';
+
+})();
