8f747174ed14c559d2b9648f12c10349b265f95a
EmeraldBoa <0arianwen0@gmail.com>
2025-03-24 03:55:49 +0100

    PB: single copy button, works

--- scripts/pathbuilder_copy button.js
+++ scripts/pathbuilder_copy button.js
@@ -5,7 +5,7 @@
 // @description  Adds a copy button to Pathbuilder's feat, feature and item descriptions.
 // @author       CertifiedDiplodocus
 // @match        http*://pathbuilder2e.com/app.html*
-// @icon         https://upload.wikimedia.org/wikipedia/commons/8/8c/Cib-archive-of-our-own_%28CoreUI_Icons_v1.0.0%29.svg
+// @icon         https://pathbuilder2e.com/favicon.ico
 // @license      GPL-3.0-or-later
 // @grant        none
 // @run-at       document-start
@@ -13,7 +13,7 @@
 
 /* TODOs
         [ ] identify location
-        [ ] jquery or not?
+        [x] jquery or not? NOT. Do I want to use it?
         [ ] create simple button & add to page
         [ ] add copy code
 
@@ -26,5 +26,92 @@
 
 (function() {
     'use strict';
+
+    // wait for body to load
+    const docObserver = new MutationObserver (() => {
+        if (document.body) {
+            buildSheetObserver()
+            docObserver.disconnect()
+        }
+    })
+    docObserver.observe (document.documentElement, {childList: true})
+
+    // wait for the user to load a character sheet (loader div is gone)
+    function buildSheetObserver() {
+        const sheetObserver = new MutationObserver (()=>{
+            const sheetIsVisible = document.getElementById('main-container').offsetParent 
+            //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
+
+            if (sheetIsVisible) {
+                modifyCharSheet()
+                sheetObserver.disconnect()
+            }
+        })
+        sheetObserver.observe (document.body, {childList: true})
+    }
+
+    function modifyCharSheet() {
+        console.log('done! Run the rest of the script')
+
+        // Character sheet section ----------------------------
+        // Feats tab
+        const featBlock = document.querySelector('.listview-item')
+        const featNameDiv = featBlock.querySelector('.top-div.listview-topdiv')
+        //const featNameDiv = document.querySelectorAll('.top-div.listview-topdiv')
+        
+        const featText = {
+            Title: featBlock.querySelector('.listview-title').textContent,
+            Info: featBlock.querySelector('.listview-detail').textContent
+        }
+        const formattedFeatText = `**${featText.Title}**
+        ${featText.Info}`
+
+        // Make button
+        const buttonTest = createButton ('div', 'div-button-simple copy-button', function(event) {copyText(formattedFeatText, event)})
+        featNameDiv.appendChild(buttonTest)
+
+    }
+
+    // &#128203; // html code for copy
+
+    //for...of (for nodelists like queryselectorall)
+
+    // copy function
+    function copyText (text, clickEvent) {
+        clickEvent.stopPropagation(); // stop from bubbling
+        console.log('copied something!')
+        copyToClipboard (text)
+    }
+
+
+    function copyToClipboard(text) {
+        var dummy = document.createElement("textarea");
+        // to avoid breaking orgain page when copying more words
+        // cant copy when adding below this code
+        // dummy.style.display = 'none'
+        document.body.appendChild(dummy);
+        //Be careful if you use textarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+        dummy.value = text;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    }
+
+/*----------------------------------------------------------------------------------------------*/
+    // Create a button with an event listener
+    function createButton (elementType, buttonClass, callbackFunctionName) {
+        const newButton = document.createElement (elementType)
+        newButton.className = buttonClass
+        newButton.textContent = '📋'
+        newButton.addEventListener ('click', callbackFunctionName, false)
+        return newButton
+    }
+
+    function createNewElement(elementType, className, textContent) {
+        const el = document.createElement(elementType);
+        el.className = className
+        el.textContent = textContent
+        return el
+    }
 
 })();
