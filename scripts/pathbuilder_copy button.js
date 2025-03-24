f908c25ca8de51f2224a88ebeed9b39b30e27251
EmeraldBoa <0arianwen0@gmail.com>
2025-03-24 22:51:33 +0100

    PB (fix depr): use navigator.clipboard.writetext

--- scripts/pathbuilder_copy button.js
+++ scripts/pathbuilder_copy button.js
@@ -8,8 +8,10 @@
 // @icon         https://pathbuilder2e.com/favicon.ico
 // @license      GPL-3.0-or-later
 // @grant        none
-// @run-at       document-start
+// @run-at       document-start 
 // ==/UserScript==
+
+//TODO: don't use document-start in header! may not need first MutationObs?
 
 /* DONE:
         [x] identify location
@@ -19,9 +21,14 @@
 
 TODO: now make it work with all elements
 
-        Steps: 
-            [ ] 1. Test adding button to feats page
-            [ ] 2. Test on sidebar
+Steps: 
+        [ ] 1. Test adding button to feats page
+        [ ] 2. Test on sidebar
+
+POLISH (late-game steps)
+        [ ] design (upload?) a subtle "copy" button to match the Pathbuilder theme
+        [ ] add action icons after the title ◇◆↩︎
+        [ ] add a visual indicator when the copy succeeds (HTML5 on the button?)
 ----------------------------------------------------------------------------------------------------------------------*/
 
 (function() {
@@ -31,27 +38,25 @@ TODO: now make it work with all elements
     const docObserver = new MutationObserver (() => {
         if (document.body) {
             buildSheetObserver()
-            docObserver.disconnect()
+            docObserver.disconnect()    // TODO: will it break if I put this first? If yes, then put the disconnect in the next function.
         }
     })
     docObserver.observe (document.documentElement, {childList: true})
 
-    // wait for the user to load a character sheet (loader div is gone)
+    // wait for the user to load a character sheet
     function buildSheetObserver() {
         const sheetObserver = new MutationObserver (()=>{
-            const sheetIsVisible = document.getElementById('main-container').offsetParent 
-            //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
-
+            const sheetIsVisible = document.getElementById('main-container').offsetParent //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
             if (sheetIsVisible) {
                 modifyCharSheet()
-                sheetObserver.disconnect()
+                sheetObserver.disconnect() // TODO: will it break if I put this first? If yes, then put the disconnect in the next function.
             }
         })
         sheetObserver.observe (document.body, {childList: true})
     }
 
+    // done! run the rest of the script.
     function modifyCharSheet() {
-        console.log('done! Run the rest of the script')
 
         // Character sheet section ----------------------------
         // Feats tab
@@ -59,59 +64,40 @@ TODO: now make it work with all elements
         const featNameDiv = featBlock.querySelector('.top-div.listview-topdiv')
         //const featNameDiv = document.querySelectorAll('.top-div.listview-topdiv')
         
-        const featText = {
+        const featText = { // TODO maybe include the action icons (emoji? ◇◆↩︎)
             Title: featBlock.querySelector('.listview-title').textContent,
             Info: featBlock.querySelector('.listview-detail').textContent
         }
-        const formattedFeatText = `**${featText.Title}**
-        ${featText.Info}`
+        const formattedFeatText =`**${featText.Title}**
+        ${featText.Info}`;
 
         // Make button
-        const buttonTest = createButton ('div', 'div-button-simple copy-button', function(event) {copyText(formattedFeatText, event)})
+        const buttonTest = createButton ('div', 'div-button-simple copy-button', function(event) {
+            event.stopPropagation() // stop from bubbling
+            copyToClipboard(formattedFeatText)
+        })
         featNameDiv.appendChild(buttonTest)
-
     }
-
-    // &#128203; // html code for copy
 
     //for...of (for nodelists like queryselectorall)
 
-    // copy function
-    function copyText (text, clickEvent) {
-        clickEvent.stopPropagation(); // stop from bubbling
-        console.log('copied something!')
-        copyToClipboard (text)
-    }
-
-
-    function copyToClipboard(text) {
-        var dummy = document.createElement("textarea");
-        // to avoid breaking orgain page when copying more words
-        // cant copy when adding below this code
-        // dummy.style.display = 'none'
-        document.body.appendChild(dummy);
-        //Be careful if you use textarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-        dummy.value = text;
-        dummy.select();
-        document.execCommand("copy");
-        document.body.removeChild(dummy);
-    }
-
 /*----------------------------------------------------------------------------------------------*/
+    async function copyToClipboard(text) {
+        try {
+            await navigator.clipboard.writeText(text);
+        } catch (error) {
+            console.error('failed to copy to clipboard'); // MAYBE: remove try/catch
+        }
+    }
+
     // Create a button with an event listener
-    function createButton (elementType, buttonClass, callbackFunctionName) {
+    function createButton (elementType, buttonClass, callbackFunction) {
         const newButton = document.createElement (elementType)
         newButton.className = buttonClass
-        newButton.textContent = '📋'
-        newButton.addEventListener ('click', callbackFunctionName, false)
+        newButton.textContent = '📋︎' // 📋📋︎
+        newButton.style.color = '#2d4059'
+        newButton.addEventListener ('click', callbackFunction, false)
         return newButton
-    }
-
-    function createNewElement(elementType, className, textContent) {
-        const el = document.createElement(elementType);
-        el.className = className
-        el.textContent = textContent
-        return el
     }
 
 })();
