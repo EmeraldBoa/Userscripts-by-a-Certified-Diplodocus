8849471601960985c982cdb23a653212cf69cbc1
EmeraldBoa <0arianwen0@gmail.com>
2025-03-28 01:48:53 +0100

    PB: sidebar fully functional, will update

--- scripts/pathbuilder_copy button.js
+++ scripts/pathbuilder_copy button.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy button for Pathbuilder
 // @namespace    https://greasyfork.org/en/users/757649-certifieddiplodocus
-// @version      0.3
+// @version      0.4
 // @description  Adds a copy button to Pathbuilder's feat, feature and item descriptions.
 // @author       CertifiedDiplodocus
 // @match        http*://pathbuilder2e.com/app.html*
@@ -22,17 +22,19 @@ TODO: now make it work with all elements
 Steps: 
         [x] 1. Test adding button to feats page
         [x] 2. Test on sidebar
-        [ ] 3. Add MutationObserver for
+        [x] 3. Add MutationObserver for
                 [x] feats
                 [x] actions
                 [x] spells 
                     [x]] > rituals! impulses! there may be more tabs for different classes...
-                [ ] sidebar during updates!
+                [x] sidebar during updates!
 
         [ ] 3b. Rejigger with promises (or fix region position)
 
         [ ] 4. Add MutationObserver for popup windows
         [ ] 5. Test with multiple open sheets
+
+        [ ] 6. (IMPORTANT) Format copied text to account for traits & source
 
 POLISH (late-game steps)
         [ ] design (upload?) a subtle "copy" button to match the Pathbuilder theme
@@ -58,16 +60,18 @@ POLISH (late-game steps)
     function modifyCharSheet() {
 
         // define the selectors for the different page regions
-        // MAYBE make a class with properties "element" and "buttonPos"
+        // MAYBE make a class with properties "element" and "buttonPos" - or define a method within the object?
         const region = {
             Sidebar: document.querySelector('#divBuildLevels'),
             Sheet: document.querySelector('.tabbed-area-display-holder'),
         }
 
-        // TODO : add observer (for when features change/are added)
+        // SIDEBAR: when any features change/are added, the entire sidebar resets, so re-add buttons
         addButtons(region.Sidebar)
+        const sidebarObserver = new MutationObserver(() => addButtons(region.Sidebar))
+        sidebarObserver.observe (region.Sidebar, {childList:true, subtree:true})
 
-        // Check if tab, character level or features change (which recreates the content of the tabbed display area)
+        // SHEET: Check if tab, character level or features change (which recreates the content of the tabbed display area)
         const tabbedAreaObserver = new MutationObserver(function() {
             if (!currentTabIsValid) {return;}
             console.log('On a valid tab! Adding buttons')
