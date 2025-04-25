cb63c0ce4d7c79b2c9b9d0439304131da8581712
EmeraldBoa <0arianwen0@gmail.com>
2025-04-25 23:43:05 +0200

    CopySeries: functional (no jquery), html cleanup

--- scripts/Copy SERIES_wip.js
+++ scripts/Copy SERIES_wip.js
@@ -6,14 +6,14 @@
 // @author       CertifiedDiplodocus
 // @match        http*://archiveofourown.org/series/*
 // @icon         data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📋</text></svg>
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
     'use strict'
     const $ = document.querySelector.bind(document) // shorthand for readability
     const $$ = document.querySelectorAll.bind(document)
-    // const parser = new DOMParser()
+    const parser = new DOMParser()
     const errPrefix = '[Copy Series Data - userscript] \n⚠ Error: '
 
     const storyCollection = {}
@@ -76,25 +76,27 @@
         const seriesInfo = {
             Title: $('h2.heading').textContent,
             Link: currentURL,
-            Description: seriesDetails[0].innerHTML || '',
-            Notes: seriesDetails[1].innerHTML || '',
+            Description: seriesDetails[0]?.innerHTML || '',
+            Notes: seriesDetails[1]?.innerHTML || '',
         }
+        trimObjectValues(seriesInfo)
         Object.assign(storyCollection, { Series: seriesInfo })
 
         // iterate through stories
-        $('.work.blurb').forEach((work, i) => {
-            const heading = work.querySelector('.header a') // maybe use $$, header[i]
-            const stats = work.querySelectAll('.stats > dd')
+        $$('.work.blurb').forEach((work, i) => {
+            const heading = work.querySelectorAll('.header a')
+            const stats = work.querySelector('.stats')
             const story = {
                 Title: heading[0].textContent,
                 Link: heading[0].href,
                 Author: heading[1].textContent,
                 Fandom: heading[3].textContent,
                 Summary: work.querySelector('.summary')?.innerHTML || '', // handle empty summaries
-                Wordcount: stats.querySelector('.words').textContent,
-                IsComplete: isAO3FicComplete(stats.querySelector('.chapters').textContent), // return boolean
-                SeriesPosition: getAO3SeriesPos(this.querySelector('.series')?.textContent || ''), // TODO handle fics in multiple series
+                Wordcount: stats.querySelector('dd.words').textContent,
+                IsComplete: isAO3FicComplete(stats.querySelector('dd.chapters').textContent), // return boolean
+                SeriesPosition: getAO3SeriesPos(work.querySelector('.series')?.textContent || ''), // TODO handle fics in multiple series
             }
+            trimObjectValues(story)
             Object.assign(storyCollection, { [`Story${i}`]: story })
         })
 
@@ -104,33 +106,35 @@
 
     // FUNCTIONS -----------------------------------------------------------------------------
 
-    function stringifyMultiple() {
-        return propertiesAndValues(
-            propertiesAndValues(storyCollection, '=', '; '),
-            ' = {',
-            '};\n\n'
-        )
+    function stringifyMultiple() { // Finally works! turn this into a nested helper function
+        const stringifiedObj = {}
+        Object.entries(storyCollection).forEach(([k, story]) => {stringifiedObj[k] = propertiesAndValues(
+            formatForAccess(story, 'Description', 'Notes', 'Summary'),
+            '=',
+            '; '
+        )})
+        return propertiesAndValues(stringifiedObj, ' = {', '};\n\n') + '};'
+    }
+
+    function iterateObject(obj, fn) { // TODO add fn arguments? or define a small function with the arguments set?
+        const newObj = {}
+        Object.entries(obj).forEach(([key, val]) => newObj[key] = fn(val))
+        return newObj
+    }
+
+    function iterateNested(obj, fn) {
+        const newObj = {}
+        Object.entries(obj).forEach(function ([key, val]) {
+            if (typeof val === 'object') { iterateNested(val, fn) }
+            newObj[key] = fn(val)
+        })
+        return newObj
     }
 
     function propertiesAndValues(storyObj, assignChar, separator) {
         return Object
             .keys(storyObj)
-            .map(function (k) { return k + assignChar + storyObj[k] }).join(separator)
-    }
-
-    // AO3 chapter format is "3/?", "31/31", "3/10"...
-    // input: "n/m" where n is a number. output: boolean
-    function isAO3FicComplete(ao3ChapterCount) {
-        ao3ChapterCount = ao3ChapterCount.trim()
-        const chaptersWritten = ao3ChapterCount.split('/')[0]
-        const chaptersTotal = ao3ChapterCount.split('/')[1]
-        return chaptersWritten === chaptersTotal
-    }
-
-    // Return position in series
-    // input: "Part n of [seriesname]". output: "n"
-    function getAO3SeriesPos(ao3SeriesInfo) {
-        return ao3SeriesInfo.replace(/^\s*Part ([\d]+) of.*/gi, '$1')
+            .map(k => k + assignChar + storyObj[k]).join(separator)
     }
 
     async function copyToClipboard(text) {
@@ -159,7 +163,115 @@
             copyAlert.classList.remove('show-alert')
         }, duration)
     }
+    // -- CLEANUP (from CopyStoryData) -----------------------------
 
+
+    // AO3 chapter format is "3/?", "31/31", "3/10"...
+    // input: "n/m" where n is a number. output: boolean
+    function isAO3FicComplete(ao3ChapterCount) {
+        ao3ChapterCount = ao3ChapterCount.trim()
+        const chaptersWritten = ao3ChapterCount.split('/')[0]
+        const chaptersTotal = ao3ChapterCount.split('/')[1]
+        return chaptersWritten === chaptersTotal
+    }
+
+    // Return position in series
+    // input: "Part n of [seriesname]". output: "n"
+    function getAO3SeriesPos(ao3SeriesInfo) {
+        return ao3SeriesInfo.replace(/^\s*Part ([\d]+) of.*/gi, '$1')
+    }
+    // CAREFUL: this can break links from other domains (e.g. whofic stories have viewstory?sid=###)
+    function cleanLink(url) {
+        return url.replace(/[?#].+/, '') // TODO add # to other urls
+    }
+
+    function trimObjectValues(obj) {
+        const trim = (x) => { return typeof x === 'string' ? x.trim() : x }
+        Object.assign(obj, iterateObject(obj, trim))
+    }
+
+    function removeThousandsSeparator(number) {
+        return number.replace(/,/gi, '')
+    }
+
+    // FIXME: regex will BREAK html like <p>this text with <em>emphasis <br>across</em> two lines</p>
+    function cleanHTML(html) {
+        if (!html.includes('<')) { return }
+        return html
+            .replaceAll(/<([/]?)b>/gi, '<$1strong>') //             - b to strong
+            .replaceAll(/<([/]?)i>/gi, '<$1em>') //                 - i to em
+            .replaceAll(/<([/]?)div>/gi, '<$1p>') //                - div to p
+            .replaceAll(/<br\s*[/]?>/gi, '</p><p>') //              - br to p // HACK: keep an eye on this one
+            .replaceAll(/(<p>){2,}|(<[/]p>){2,}/gi, '$1$2') //      - discard wrappers (<p><p>, </p></p>)
+            .replaceAll(/\s+(<\/p>)|(<p>)\s+/gi, '$1$2') //         - no white space around paragraphs (do I need this?)
+            .replaceAll(/(<p><\/p>){2,}/gi, '<p></p>') //           - max one empty paragraph
+            .replaceAll(/^<p><\/p>|<p><\/p>$/gi, '') //             - delete blank start/end paragraphs
+    }
+
+    // ACCESS EDITING ------------------------------------------------------
+    // Format HTML text (properties such as 'description', 'notes', 'summary')
+    function formatForAccess(obj, ...targetProperties) {
+        const objForAccess = structuredClone(obj)
+        for (const prop of targetProperties) {
+            if (!objForAccess[prop]) { continue }
+            let formattedSummary = blockquoteToItalics(objForAccess[prop])
+            formattedSummary = hangingIndents(formattedSummary)
+            formattedSummary = formattedSummary.replaceAll(/<([/]?)p>/gi, '<$1div>') // p to div (Access default)
+            objForAccess[prop] = formattedSummary
+        }
+        return objForAccess
+    }
+
+    function applySuccessiveFunctions(target, ...fns) {
+        let transformedTarget = target
+        for (const fn of fns) {
+            transformedTarget = fn(transformedTarget)
+        }
+        fns.forEach(fn => target = fn(target))
+        return transformedTarget
+    }
+
+    class storyForAccess {
+        constructor(storyObj) {
+            return structuredClone(storyObj)
+        }
+
+        get blockquoteToItalics() { return blockquoteToItalics(this) }
+        get hangingIndents() { return hangingIndents(this) }
+        get pToDiv() { return this.replaceAll(/<([/]?)p>/gi, '<$1div>') }
+    }
+
+    // Invert the italics within a blockquote. Remove the blockquote tags and wrap in empty paragraphs. Return HTML string
+    function blockquoteToItalics(html) { // MAYBE reformat this to use the parsed summary
+        const bqArray = html.split(/<[/]?blockquote>/gi)
+        if (bqArray.length < 2) { return html } // no blockquotes found
+
+        for (let i = 1; i < bqArray.length; i += 2) { // iterate through bqArray[1], [3], [5]... (blockquoted text)
+            bqArray[i] = bqArray[i] //                      <em>,  </em>
+                .replaceAll(/(<[/]?)em>/gi, '$1/em>') //    </em>, <//em>
+                .replaceAll(/<[/]{2}/gi, '<') //            </em>, <em>
+                .replaceAll('<p>', '<p><em>').replaceAll('</p>', '</em></p>')
+                .replaceAll(/<em>(\s+)<\/em>/gi, '$1')
+        }
+        return bqArray.join('<p></p>') // wrap blockquoted text in empty paragraphs
+            .replaceAll(/(<p><\/p>){2,}/gi, '<p></p>') // max two empty paragraphs
+            .replaceAll(/^<p><\/p>|<p><\/p>$/gi, '') //   no empty paragraph at start or end
+    }
+
+    // Indent all paragraphs but the first in every block (i.e. not after a blank paragraph). Return HTML string
+    function hangingIndents(html) {
+        const summary = parser.parseFromString(html, 'text/html').body
+        const indentString = '\xa0'.repeat(4)
+        let pCount = 0 // count paragraphs in sections (1 = first non-empty)
+        summary.querySelectorAll('p').forEach((p) => {
+            pCount++
+            if (!p.textContent) pCount = 0 // reset on empty paragraphs (new block)
+            if (pCount < 2) return // don't indent the first paragraph
+            p.prepend(indentString)
+        })
+        return summary.innerHTML
+    }
+    // ------------------------------------------------------------------
     // eslint-disable-next-line no-undef
     GM_addStyle (`
     .copy-alert {
